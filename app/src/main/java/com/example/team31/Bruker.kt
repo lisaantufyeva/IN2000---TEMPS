3ec6c3a53189a6f2f9fee08b8eb9dae6bd3d731b
Gregor Askjer <gregoras@ifi.uio.no>
2021-05-04 18:36:17 +0200

    Nå lagres både longitude og latitude separat.

--- app/src/main/java/com/example/team31/Bruker.kt
+++ app/src/main/java/com/example/team31/Bruker.kt
@@ -2,8 +2,8 @@ package com.example.team31
 
 import java.io.Serializable
 
-data class Bruker(var id: String?, var email: String?, var passord: String?, var bilde: String?, var navn:String?, var latlng:String?, var stedNavn:String?, var normalBemanning:String?, var maxBemanning:String?, var triggerTemp:String?, var nedbor: Boolean, var aapenFra:String?, var aapenTil:String?):Serializable{
-    constructor() : this("1", "e", "1", "1", "e", "e", "e","e", "e", "e", false, "e", "e")
+data class Bruker(var id: String?, var email: String?, var passord: String?, var bilde: String?, var navn:String?, var latitude:String?,var longitude:String?, var stedNavn:String?, var normalBemanning:String?, var maxBemanning:String?, var triggerTemp:String?, var nedbor: Boolean, var aapenFra:String?, var aapenTil:String?):Serializable{
+    constructor() : this("1", "e", "1", "1", "e", "e", "e","e", "e", "e", "e", false, "e", "e")
 }
 
 
